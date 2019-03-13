368082600c2bd92945792daa6f72063ba8eace67
11figure <1figure@gmail.com>
2019-03-14 00:58:51 +0800

    test

---
+++ src/components/MyHelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>我的标题</div>
+        <div>{{ test }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyHelloWorld',
+        data () {
+            return{
+                test:'MyTestPage'
+            } 
+        }
+    }
+</script>
